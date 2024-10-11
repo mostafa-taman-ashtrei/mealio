7c2e6c5e0426be001293355f0f0d5ee7680ff485
Mostafa Taman <mostafa.taman.me@gmail.com>
2024-10-11 03:06:52 +0300

    style: restyle the auth pages

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { SignIn as ClerkSignUp } from "@clerk/nextjs";
+import Image from "next/image";
+import Logo from "@/assets/logos/logo.png";
 import { dark } from "@clerk/themes";
 import { useTheme } from "next-themes";
 
@@ -9,9 +11,22 @@ const SignIn: React.FC = () => {
     const clerkTheme = resolvedTheme === "dark" ? { baseTheme: dark } : undefined;
 
     return (
-        <div className="flex justify-center h-screen py-24">
-            <ClerkSignUp appearance={clerkTheme} afterSignOutUrl="/" />
-        </div>
+        <section className="p-6">
+            <div className="container grid gap-6 mx-auto text-center lg:grid-cols-2 xl:grid-cols-5">
+                <div className="w-full px-6 py-16 rounded-md sm:px-12 md:px-16 xl:col-span-2 ">
+                    <ClerkSignUp appearance={clerkTheme} />
+                </div>
+
+                <div className="object-cover w-full rounded-md xl:col-span-3">
+                    <Image
+                        src={Logo}
+                        width={1000}
+                        height={1000}
+                        alt="login image"
+                    />
+                </div>
+            </div>
+        </section>
     );
 };
 
